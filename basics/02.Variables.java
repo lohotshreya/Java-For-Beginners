bb62f9c3ee26ed159828b307c42c644057a8943d
lohotshreya <lohotshreya@gmail.com>
2026-01-20 17:14:06 +0530

    Add Variables class with main method

---
+++ basics/02.Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 19;
+        double cgpa = 8.5;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
