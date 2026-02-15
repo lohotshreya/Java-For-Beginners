36c4a409c83e0ebffc1bd7920762da240a78f8ed
lohotshreya <lohotshreya@gmail.com>
2026-02-15 20:49:02 +0530

    Add PalindromeNumber class to check for palindromes

---
+++ basics/09.PalindromeNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int original = num;
+        int reversed = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        if (original == reversed) {
+            System.out.println("Number is a palindrome");
+        } else {
+            System.out.println("Number is not a palindrome");
+        }
+
+        sc.close();
+    }
+}
